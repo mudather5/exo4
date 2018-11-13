226585f6d19d26e9d348f36f27d8c7b7fc000ab3
mudather5 <mudathermohamed50@gmail.com>
2018-11-13 22:14:54 +0100

    added css propreties

--- js/main.js
+++ js/main.js
@@ -7,54 +7,45 @@ let Clients = {
                 for (const key in object) {
                         if (object.hasOwnProperty(key)) {
                                 this[key] = object[key];
+                                console.log(this.name);
+
                         }
                 }
 
         }
+
+
 };
 
 
 
 let htmlManager = {
+
         thead: document.getElementById('myTable'),
-        getTr: function (object) {
-                //create new tr element
-                let tr = document.createElement('tr');
-                htmlManager.thead.appendChild(tr);
-        },
 
         getTdid: function (object) {
-                //create new td in the table
-                let tr = document.createElement('tr');
-                htmlManager.thead.appendChild(tr);
-                tdId = document.createElement('td');
-                tdId.innerHTML = object[1].id;
-                tr.appendChild(tdId);
-        },
 
-        gettdName: function (object) {
-                let tr = document.createElement('tr');
-                htmlManager.thead.appendChild(tr);
-                let tdName = document.createElement('td');
-                tdName.innerHTML = object[1].name;
-                tr.appendChild(tdName);
-        },
+                for (let i = 0; i < object.length; i++) {
 
-        getTdage: function (object) {
-                let tr = document.createElement('tr');
-                htmlManager.thead.appendChild(tr);
-                let tdAge = document.createElement('td');
-                tdAge.innerHTML = object[1].age;
-                tr.appendChild(tdAge);
+                        //create new td in the table
+                        let tr = document.createElement('tr');
+                        htmlManager.thead.appendChild(tr);
+                        tdId = document.createElement('td');
+                        tdId.innerHTML = object[i].id;
+                        tr.appendChild(tdId);
+                        let tdName = document.createElement('td');
+                        tdName.innerHTML = object[i].name;
+                        tr.appendChild(tdName);
+                        let tdAge = document.createElement('td');
+                        tdAge.innerHTML = object[i].age;
+                        tr.appendChild(tdAge);
+                        let tdCity = document.createElement('td');
+                        tdCity.innerHTML = object[i].city;
+                        tr.appendChild(tdCity);
 
-        },
 
-        getTdCity: function (object) {
-                let tr = document.createElement('tr');
-                htmlManager.thead.appendChild(tr);
-                let tdCity = document.createElement('td');
-                tdCity.innerHTML = object[1].city;
-                tr.appendChild(tdCity);
+
+                }
         }
 
 
@@ -65,11 +56,11 @@ let xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
                 let myObject = JSON.parse(this.responseText);
+                Clients.getClient(myObject);
                 htmlManager.getTdid(myObject);
                 htmlManager.gettdName(myObject);
                 htmlManager.getTdage(myObject);
                 htmlManager.getTdCity(myObject);
-                // htmlManager.getTr(myObject);
 
 
         }
